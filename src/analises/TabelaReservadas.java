b946fcdb81a2fdcca9d8032998d2a891f8c322c0
SauloNCunha <saulonacimentocunha@hotmnail.com>
2019-06-10 20:59:13 -0300

    Adicionado palavras reservadas

--- src/analises/TabelaReservadas.java
+++ src/analises/TabelaReservadas.java
@@ -20,6 +20,22 @@ public class TabelaReservadas {
         tabela.put(2, "uses");
         tabela.put(3, "begin");
         tabela.put(4, "end");
+        tabela.put(5, "if");
+        tabela.put(6, "then");
+        tabela.put(7, "else");
+        tabela.put(8, "for");
+        tabela.put(9, "to");
+        tabela.put(10, "do");
+        tabela.put(11, "while");
+        tabela.put(12, "repeat");
+        tabela.put(13, "until");
+        tabela.put(14, "const");
+        tabela.put(15, "var");
+        tabela.put(16, "define");
+        tabela.put(17, "use");
+        tabela.put(18, "key");
+        tabela.put(19, "readkey");   
+        
     }
     
     public Boolean estaContido(String valor) {
